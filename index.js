04abfe4ff58b6591d7b42b7808ce292fe76c48f3
Danil Kincharov <donvietnam@178-21-11-152.cloudvps.regruhosting.ru>
2022-06-14 17:47:48 +0700

    add: main routes

--- index.js
+++ index.js
@@ -2,10 +2,104 @@ const fs = require("fs");
 const path = require("path");
 const { render } = require("./generate");
 const http = require("http");
+const crypto = require('crypto');
+
+
+const templates = new Set(['Общая информация']);
+const cases = new Map();
+const stages = new Set(['В ОЖИДАНИИ', 'В ПРОЦЕССЕ', 'НА ПРОВЕРКЕ', 'ВЫПОЛНЕНО', 'ИСПРАВЛЕНИЕ']);
+const stageTemplate = new Map();
+const caseValues = new Map();
+const last = 'ВЫПОЛНЕНО';
+const caseFile = new Map();
+
+const getBody = async (request) => {
+  const buffers = [];
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+  
+  return Buffer.concat(buffers);
+};
+
+const parseBody = async (request) => {
+  const buffer = await getBody(request);
+  return JSON.parse(buffer.toString('utf-8'));
+};
+
+const router = {
+  'case/add': async ( {name} ) => {
+    const id = crypto.randomUUID();
+    const newCase = {name, id, stage: 'В ОЖИДАНИИ'};
+    cases.set(id, newCase);
+    return newCase;     
+  },
+  
+  'stage/addTemplate': async ({stage, templateName}) => {
+    if (stages.has(stage) && templates.has(templateName)) {
+      stageTemplate.set(stage, templateName)
+      return {stage, templateName, author: 'danil'};
+    } else {
+      return null;
+    }
+  },
+  
+  'case/addValue': async ({caseId, values}) => {
+    if(cases.has(caseId)) {
+      prevValues = caseValues.has(caseId) ? caseValues.get(caseId) : {};
+      caseValues.set(caseId, {...prevValues, ...values});
+      const filled = Object.keys({...prevValues, ...values}).length;
+      const left = 10 - filled; 
+      return {caseId, filled, left };
+    }
+    return null;
+  },
+  
+  'case/changeStage': async ({caseId, stage}) => {
+    if (cases.has(caseId) && stages.has(stage)) {
+      const caseObject = cases.get(caseId);
+      caseObject.stage = stage;
+      const generatedDocuments = [stageTemplate.get(stage)];
+      caseFile.set(caseId, render(caseValues.get(caseId)));
+      return {caseId, stage, final: stage === last, generatedDocuments}
+    }
+    
+    return null;
+  },
+  
+  'case/getFile': async ({caseId, name}) => {
+    if (caseFile.has(caseId) && templates.has(name)) {
+      return {file: true, binary: caseFile.get(caseId)};
+    }
+    
+    return null;
+  }
+};
+
+const callMethod = async (data) => {
+  const result = await router[data.method](data.params);
+  if (result && result.file) {
+    return result;
+  }
+  return {id: data.id, result};
+};
 
 const server = http.createServer(async (request, response) => {
-	response.statusCode = 200;
-	response.end("Hello!");
+	try {
+  	  const body = await parseBody(request);
+      const result = await callMethod(body);
+      response.statusCode = 200;
+      if (result.file) {
+        response.setHeader('content-disposition', "attachment; filename=document.docx");
+        response.end(result.binary);    
+      } else {
+        response.end(JSON.stringify(result));     
+      }
+		} catch (error) {
+		  console.log(error);
+	    response.statusCode = 200;
+	    response.end(JSON.stringify({error: 'Internal server error.'}));  
+	  }
 });
 
 // const result = render({
@@ -23,4 +117,4 @@ const server = http.createServer(async (request, response) => {
 
 // fs.writeFileSync(path.resolve(__dirname, "output.docx"), result);
 
-server.listen(8088, "127.0.0.1");
+server.listen(8088);
